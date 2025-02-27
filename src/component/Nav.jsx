e0bc31f8cf19490f5a63c5bdf3b762641c7632ae
Xori <xori93@aol.com>
2025-02-26 22:38:33 -0500

    working on the nav drop down

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Logo from "../assests/Logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Nav = () => {
   return (
@@ -14,6 +15,15 @@ const Nav = () => {
           </li>
           <li className="nav__list">
             <a href="/" className="nav__link">Clothing</a>
+          </li>
+          <button className="btn__menu">
+            <FontAwesomeIcon icon="bars" />
+          </button>
+          <li className="nav__icon">
+            <a href="/cart" className="nav__link">
+              <FontAwesomeIcon icon="shopping-cart" />
+            </a>
+            <span className="cart__lenght">2</span>
           </li>
         </ul>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faTimes, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faTimes, faShoppingCart);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
